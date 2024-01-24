4536c16a70b78005343644382b1a5f56682fe867
devjohnpaul <120043178+labanonpaul@users.noreply.github.com>
2024-01-24 12:29:13 +0800

    update variables

---
+++ variables/main.py
@@ -0,0 +1,18 @@
+# What is a Variables?
+
+# Variables is used to store data.
+
+# A variable give a name to a piece of data.
+# Like a box with a label, it tells you what's
+# inside the box.
+
+
+name = "John Doe"
+age = 99
+address = "Calixto Street"
+Single = True
+
+print(name)
+print(age)
+print(address)
+print(Single)
